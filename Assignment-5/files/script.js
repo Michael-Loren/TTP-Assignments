5ab4aaafecf25cba7c9326c5b84cb3ed6891a902
Michael-Loren <michael.loren@cix.csi.cuny.edu>
2022-01-07 15:14:16 -0500

    Start scripting

--- Assignment-5/files/script.js
+++ Assignment-5/files/script.js
@@ -1,1 +1,13 @@
-//update row independently
+const buttons = document.getElementsByClassName("btn");
+
+const btnAddRow = buttons[1];
+const btnDelRow = buttons[2];
+const btnAddCol = buttons[3];
+const btnDelCol = buttons[4];
+
+
+
+console.log(btnAddRow);
+
+btnAddRow.addEventListener("click", () => console.log("Hello"))
+
