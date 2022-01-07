e81a7c5d996dc0321a0e144c730c53ddf31e58f0
Michael-Loren <michael.loren@cix.csi.cuny.edu>
2022-01-07 16:47:18 -0500

    Add logic for rows

--- Assignment-5/files/script.js
+++ Assignment-5/files/script.js
@@ -7,7 +7,46 @@ const btnDelCol = buttons[4];
 
 
 
-console.log(btnAddRow);
 
-btnAddRow.addEventListener("click", () => console.log("Hello"))
 
+
+const rows = document.getElementsByClassName("row");
+
+
+let colCount = 4; //change later
+
+
+
+
+
+
+
+
+function addRow(el){
+    const rowdiv = document.createElement(`div`)
+    rowdiv.className = "row";
+    //populate empty row
+    for(let i = 0; i < colCount; i++){
+        const col = document.createElement(`div`);
+        col.className = "col-sm border border-3 border-dark box white";
+        rowdiv.appendChild(col) //append columns
+    }
+    el.appendChild(rowdiv);
+
+}
+
+
+console.log(rows);
+
+
+
+
+
+console.log(`colCount: ${colCount}`);
+
+
+btnAddRow.addEventListener("click", () => {
+    addRow(document.querySelector(".content"))
+} )
+
+btnDelRow.addEventListener("click", () => rows[rows.length-1].remove())
