d54a2e93ff9d12638e87d6b56fa21a81a7c6680b
Michael-Loren <michael.loren@cix.csi.cuny.edu>
2022-01-11 16:19:02 -0500

    get api working

--- giphy-thing/giphy-thing/src/App.js
+++ giphy-thing/giphy-thing/src/App.js
@@ -6,6 +6,8 @@ import './App.css';
 function App() {
   const [searchText, setSearchText] = useState("");
   const [displayText, setDisplayText] = useState("");
+  const [trendQuery, setTrendQuery] = useState([]);
+  const [loading, setLoading] = useState(true);
   function onChange(event){
     setSearchText(event.target.value);
   }
@@ -15,8 +17,23 @@ function App() {
     event.preventDefault();
   }
   
-  
+  async function fetchAPI() {
+    try {const response = await fetch(`http://api.giphy.com/v1/gifs/trending?api_key=${process.env.REACT_APP_API_KEY}&limit=9`)
+    const data = await response.json();
+    setTrendQuery(data.data);
+    console.log(data.data[0]);
+    setLoading(false);
+  }
+  catch (error){
+    console.log(error);
+  }
+  }
 
+    useEffect(async () => {
+      fetchAPI();
+    }, [])
+
+//console.log("Hello", trendQuery[0].images.downsized_medium.url)
   return (
     <div className="App">
       <header className="App-header">
@@ -24,7 +41,10 @@ function App() {
         <SearchForm onClick={onClick} onChange={onChange} />
       </header>
       <main>
-        <Card content={displayText}/>
+        {
+        !loading ? trendQuery.map((data) => {return <Card url={data}/>})
+        : <p>Didn't work</p>
+        }
       </main>
     </div>
   );
--- giphy-thing/giphy-thing/src/components/Card.js
+++ giphy-thing/giphy-thing/src/components/Card.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Card(props) {
     
-
+console.log(props.url);
     return (
         <div>
-            <p>{props.content}</p>
+            <img src={props.url.images.downsized_medium.url}></img>
         </div>
     )
 }
